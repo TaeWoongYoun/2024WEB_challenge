d4b29b26b82ccefa5632cfc20e038e72f4d83e04
xodnd0507 <24457545yong@gmail.com>
2024-08-18 13:18:02 +0900

    test : Update

--- Gyeonggi/Desgin/seong.js
+++ Gyeonggi/Desgin/seong.js
@@ -2,7 +2,7 @@ let question = [];
 let locationIndex = 0;
 let index = 0;
 let completedCourse = {};
-let isFIleLoaded = false;
+let isFileLoaded = false;
 
 fetch('quiz.json')
     .then(response => response.json())
@@ -12,10 +12,10 @@ fetch('quiz.json')
         window.addEventListener('DOMContentLoaded', eventChange());
     });
 
-const toggleModal = show => document.querySelector('.modal').classList.toggle('show-modal', show);
-document.querySelector('coupon').addEventListener('click', () => {
+const toggleModal = show => document.querySelector('.modal').classList.toggle('show-modal', show)
+document.querySelector('.coupon').addEventListener('click', () => {
     if (document.querySelector('.quiz-box').classList.contains('show-box')) {
-        alert("이미 퀴즈가 시작되어서 쿠폰을 발급 받을 수 없습니다");
+        alert("이미 퀴즈가 시작되어서 쿠폰을 발급 받을 수 없습니다.");
     } else {
         toggleModal(true);
     }
