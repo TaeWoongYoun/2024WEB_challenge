cddadab38178a81894e84c31d39027744024b549
xodnd0507 <24457545yong@gmail.com>
2024-08-18 20:00:36 +0900

    test : Update

--- Gyeonggi/Desgin/seong.js
+++ Gyeonggi/Desgin/seong.js
@@ -1,6 +1,6 @@
 let question = [];
-let index = 0;
 let locationIndex = 0;
+let index = 0;
 let completedCourse = {};
 let isFileLoaded = false;
 
@@ -9,17 +9,17 @@ fetch('quiz.json')
     .then(data => {
         question = data;
         initMap();
-        window.addEventListener('DOMContentLoaded', eventChange());
-    });
+        window.addEventListener('DOMContentLoaded', eventChange())
+    })
 
 const toggleModal = show => document.querySelector('.modal').classList.toggle('show-modal', show);
 document.querySelector('.coupon').addEventListener('click', () => {
-    if (document.querySelector('.quiz-box').classList.contains('show-box')) {
-        alert("퀴즈가 이미 진행중이라서 쿠폰을 발급 받을 수 없습니다.")
+    if(document.querySelector('.quiz-box').classList.contains('show-box')){
+        alert("퀴즈가 이미 시작되어서 쿠폰 발급 불가");
     } else {
-        toggleModal(true)
+        toggleModal(true);
     };
-});
+})
 document.querySelector('.close-btn').addEventListener('click', () => toggleModal(false));
 
 document.querySelector('.submit').addEventListener('click', () => {
@@ -31,9 +31,9 @@ document.querySelector('.submit').addEventListener('click', () => {
         document.getElementById('canvas').height = image.height;
         ctx.drawImage(image, 0, 0)
         ctx.fillText(document.getElementById('name').value, canvas.width - 185, canvas.height - 305)
-        ctx.fillText(new Date().toISOString().split('T')[0], canvas.width - 185, canvas.height - 355);
+        ctx.fillText(new Date().toISOString().split('T')[0], canvas.width - 185, canvas.height - 355)
         const link = document.createElement('a')
-        link.href = document.getElementById('canvas').toDataURL('image/png');
+        link.href = document.getElementById('canvas').toDataURL('image/png')
         link.download = 'stamp_card.png';
         link.click();
     }
@@ -41,8 +41,8 @@ document.querySelector('.submit').addEventListener('click', () => {
 
 const startQuiz = () => {
     const selectedCourse = document.querySelector('.select').value;
-    if (completedCourse[selectedCourse]) {
-        alert("이미 완료한 코스")
+    if (completedCourse[selectedCourse]){
+        alert("이미 완료한 코스입니다.")
     } else if (!isFileLoaded) {
         document.getElementById('file-input').click();
     } else {
@@ -50,21 +50,21 @@ const startQuiz = () => {
         loadQuestion();
     }
 }
-document.querySelector('.quiz-start').addEventListener('click', startQuiz)
+document.querySelector('.quiz-start').addEventListener('click', startQuiz);
 document.getElementById('file-input').addEventListener('change', event => {
     const file = event.target.files[0];
-    if (file && file.name === 'stamp_card.png') {
+    if (file && file.name === 'stamp_card.png'){
         document.getElementById('this-file').innerHTML = file.name;
         isFileLoaded = true;
         startQuiz();
     } else {
-        alert("내가 발급해준 쿠폰만.")
+        alert("내가 발급한 쿠폰만 써야지")
     }
 })
 
 const loadQuestion = () => {
     const {idx, question : dataQuestion, correct, incorrect} = question[locationIndex].quiz[index];
-    const allAnswer = [correct, ...incorrect].sort(() => Math.random() - 0.5);
+    const allAnswer = [correct, ...incorrect].sort(() => Math.random() - 0,5);
 
     document.querySelector('.quiz-box').innerHTML = `
         <div>
@@ -72,6 +72,6 @@ const loadQuestion = () => {
             <p>${dataQuestion}</p>
             ${allAnswer.map(item => `<button class="answer">${item}</button>`).join('')}
         </div>`;
-
-    document.querySelectorAll('.answer').forEach(btn => btn.addEventListener('click', () => handleAnswer(btn.textContent = correct)))
+    
+    document.querySelectorAll('.answer').forEach(btn => btn.addEventListener('change', changeAnswer(btn.textContent = correct)))
 }
