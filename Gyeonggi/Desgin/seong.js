cb49638aea51f1d27cc28812e09e89a6aac8951c
xodnd0507 <24457545yong@gmail.com>
2024-08-21 10:38:18 +0900

    test : Update

--- Gyeonggi/Desgin/seong.js
+++ Gyeonggi/Desgin/seong.js
@@ -13,16 +13,16 @@ document.addEventListener('DOMContentLoaded', () => {
             eventChange();
         })
 
-    const toggleModal = show => document.querySelector('.modal').classList.toggle('show-modal', show);
+    const toggleModal = show => document.querySelector('.modal').classList.toggle('show-modal', show)
     document.querySelector('.coupon').addEventListener('click', () => {
         if (document.querySelector('.quiz-box').classList.contains('show-box')) {
-            alert("퀴즈가 이미 시작되어 쿠폰을 발급 받을 수 없습니다.")
+            alert('퀴즈가 이미 시작되어 쿠폰을 발급 받을 수 없습니다.')
         } else {
             toggleModal(true);
         }
-    })
+    });
     document.querySelector('.close-btn').addEventListener('click', () => toggleModal(false));
-    
+
     document.querySelector('.submit').addEventListener('click', () => {
         const ctx = document.getElementById('canvas').getContext('2d');
         const image = new Image();
@@ -31,19 +31,19 @@ document.addEventListener('DOMContentLoaded', () => {
             document.getElementById('canvas').width = image.width;
             document.getElementById('canvas').height = image.height;
             ctx.drawImage(image, 0, 0);
-            ctx.fillText(document.getElementById('name').value, canvas.width - 185, canvas.height - 305)
-            ctx.fillText(new Date().toISOString().split('T')[0], canvas.width - 185, canvas.height - 355)
+            ctx.fillText(document.getElementById('name').value, canvas.width - 185, canvas.height - 305) ;
+            ctx.fillText(new Date().toISOString().split('T')[0], canvas.width - 185, canvas.height - 355);
             const link = document.createElement('a');
             link.href = document.getElementById('canvas').toDataURL('image/png');
-            link.download = 'stamp_card.png'
+            link.download = 'stamp_card.png';
             link.click();
-        }
-    })
+        };
+    });
 
     const startQuiz = () => {
         const selectedCourse = document.querySelector('.select').value;
         if (completedCourse[selectedCourse]) {
-            alert("이미 완주한 코스입니다.");
+            alert('이미 완주한 코스입니다')
         } else if (!isFileLoaded) {
             document.getElementById('file-input').click();
         } else {
@@ -58,11 +58,10 @@ document.addEventListener('DOMContentLoaded', () => {
         if (file && file.name === 'stamp_card.png') {
             document.getElementById('this-file').innerHTML = file.name;
             isFileLoaded = true;
-            startQuiz();
         } else {
-            alert("우리가 발급한 스탬프 카드만 사용하세요.")
+            alert('우리가 발급한 쿠폰을 사용해주세요.')
         }
-    })
+    });
 
     const loadQuestion = () => {
         const {idx, question: dataQuestion, correct, incorrect} = question[locationIndex].quiz[index];
@@ -75,58 +74,58 @@ document.addEventListener('DOMContentLoaded', () => {
             ${allAnswers.map(item => `<button class="answer">${item}</button>`).join('')}
         </div>`;
 
-        document.querySelectorAll('.answer').forEach(btn => btn.addEventListener('click', () => handleAnswer(btn.textContent === correct)))
-    }
+        document.querySelectorAll('.answer').forEach(btn => btn.addEventListener('click', () => handleAnswer(btn.textContent == correct)));
+    };
 
     const handleAnswer = isCorrect => {
         document.querySelector('.result-box').textContent = isCorrect ? '정답입니다!' : '오답입니다~ 다시 풀어보세요!';
-
+        
         if (isCorrect) {
             index++;
             document.querySelectorAll('.waypoint').forEach(waypoint => {
-                if (index == waypoint.innerHTML) waypoint.classList.add('complete-way')
-            })
+                if (index == waypoint.innerHTML) waypoint.classList.add('complete-way');
+            });
         }
 
-        document.querySelector('.result-box').style.display = 'block'
-        document.querySelector('.quiz-box').style.display = 'none'
-
+        document.querySelector('.result-box').style.display = 'block';
+        document.querySelector('.quiz-box').style.display = 'none';
+        
         setTimeout(() => {
             if (index < question[locationIndex].quiz.length) {
                 loadQuestion()
             } else {
-                finishQuiz();
+                finishQuiz()
             }
-            document.querySelector('.result-box').style.display = 'none'
-            document.querySelector('.quiz-box').style.display = 'block'
+            document.querySelector('.result-box').style.display = 'none';
+            document.querySelector('.quiz-box').style.display = 'block';
         }, 1000)
     }
 
     const finishQuiz = () => {
         const selectedCourse = document.querySelector('.select').value;
-        completedCourse[selectedCourse] = true;
         index = 0;
-        document.querySelector('.quiz-box').classList.remove('show-box')
-        alert(Object.keys(completedCourse).length == 3 ? '모든 코스를 완주했습니다.' : '해당 코스를 완주했습니다.')
+        completedCourse[selectedCourse] = true;
+        document.querySelector('.quiz-box').classList.remove('show-box');
+        alert(Object.keys(completedCourse).length == 3 ? '모든 코스를 완주했습니다' : '해당 코스를 완주했습니다.');
         if (Object.keys(completedCourse).length == 3) {
             document.getElementById('this-file').innerHTML = '';
             isFileLoaded = false;
         }
-    }
+    };
 
     const initMap = () => {
         updateMap(document.querySelector('.select').value);
         eventChange();
     }
 
-    document.querySelector('.select').addEventListener('change', function() {
-        updateMap(this.value)
+    document.querySelector('.select').addEventListener('change', function(){
+        updateMap(this.value);
         eventChange();
     })
 
     const updateMap = value => {
-        document.getElementById('map-image').src = `map/${value}.png`
-        locationIndex = ['창덕궁', '경복궁', '신라'].indexOf(value)
+        document.getElementById('map-image').src = `map/${value}.png`;
+        locationIndex = ['창덕궁', '경복궁', '신라'].indexOf(value);
     }
 
     const pathCanvas = document.createElement('canvas')
@@ -134,44 +133,28 @@ document.addEventListener('DOMContentLoaded', () => {
 
     const setCanvasSize = () => {
         Object.assign(pathCanvas, {
-            width: document.querySelector('.map').offsetWidth,
-            height: document.querySelector('.map').offsetHeight,
-            style: 'position:absolute; top:-15px; left:-15px;'
+            width : document.querySelector('.map').offsetWidth,
+            height : document.querySelector('.map').offsetHeight,
+            style : 'position:absolute; left:-15px; top:-15px;'
         })
     }
 
     const drawPaths = waypoint => {
         const ctx = pathCanvas.getContext('2d');
-        ctx.clearRect(0, 0, pathCanvas.width, pathCanvas.height)
+        ctx.clearRect(0, 0, pathCanvas.width, pathCanvas.height);
         if (waypoint.length > 1) {
             ctx.beginPath();
             waypoint.forEach((wp, i) => {
-                [x,y] = [wp.offsetLeft + wp.offsetWidth / 2, wp.offsetTop + wp.offsetHeight / 2]
+                [x,y] = [wp.offsetLeft + wp.offsetWidth / 2, wp.offsetTop + wp.offsetHeight / 2];
                 i === 0 ? ctx.moveTo(x,y) : ctx.lineTo(x,y)
             })
-            ctx.strokeStyle = '#111'
-            ctx.lineWidth = 5
-            ctx.stroke()
+            ctx.strokeStyle = '#111';
+            ctx.lineWidth = 5;
+            ctx.stroke();
         }
     }
 
     const eventChange = () => {
-        const selectedCourse = document.querySelector('.select').value;
-        document.querySelectorAll('.waypoint').forEach(wp => wp.remove());
-        const waypoints = question[locationIndex].quiz.map(ping => {
-            const way = document.createElement('div')
-            way.className = 'waypoint'
-            Object.assign(way.style, {
-                position: 'absolute',
-                left: `${ping.location[0]}px`,
-                top: `${ping.location[1]}px`
-            })
-            way.innerHTML = ping.idx;
-            if(completedCourse[selectedCourse]) way.classList.add('complete-way')
-            document.querySelector('.map').appendChild(way)
-            return way;
-        })
-        setCanvasSize()
-        drawPaths(waypoints)
+        
     }
 })
